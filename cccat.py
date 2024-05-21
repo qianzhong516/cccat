be16b72115e9470a88222584c6ba44bd5867fc59
qianzhong516 <zhongqian516@gmail.com>
2024-05-21 11:46:15 +0200

    clean up & hand file i/o in one place

--- cccat.py
+++ cccat.py
@@ -1,16 +1,15 @@
 import sys
 import argparse
-import os.path
 
 def main():
     parser = argparse.ArgumentParser(description='A copy of the command line tool cat.')
-    parser.add_argument('filenames', type=str, nargs='*', help='accepts a filename to show the content from')
-    parser.add_argument('-n', dest='mark_line_number', action='store_true', help='switch on/off line numbers along with the content to show')
-    parser.add_argument('-b', dest='mark_non_blank_lines', action='store_true', help='mark line numbers along with non-blank lines only')
+    parser.add_argument('filenames', type=str, nargs='*', help='the files to read')
+    parser.add_argument('-n', action='store_true', help='number all output lines')
+    parser.add_argument('-b', action='store_true', help='number non-blank output lines')
     args = parser.parse_args()
     
-    mark_non_blank_lines = args.mark_non_blank_lines
-    mark_line_number = args.mark_line_number or mark_non_blank_lines
+    mark_non_blank_lines = args.b
+    mark_line_number = args.n or mark_non_blank_lines
         
     if len(args.filenames) == 0 or args.filenames[0] == '-':
         try:
@@ -21,15 +20,17 @@ def main():
         return
     
     for path in args.filenames:
-        if not os.path.exists(path):
-            print(f'cccat: {path}: No such file or directory')
-            continue
         read_file(path, mark_line_number, mark_non_blank_lines)
 
 
 def read_file(path, mark_line_number, mark_non_blank_lines):
-    with open(path, 'r') as file:
-        custom_print(file, mark_line_number, mark_non_blank_lines)
+    try:
+        with open(path, 'r') as file:
+            custom_print(file, mark_line_number, mark_non_blank_lines)
+    except FileNotFoundError:
+        print(f'cccat: {path}: No such file or directory')
+    except IOError as e:
+        print(f'cccat: {path}: I/O error({e.errno}): {e.strerror}')
         
         
 def custom_print(lines, mark_line_number, mark_non_blank_lines):
